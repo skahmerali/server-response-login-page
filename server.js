a1b45cec04620777a15cbb67aaf800288796a6f5
skahmerali <38328235+skahmerali@users.noreply.github.com>
2020-12-24 01:36:57 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+var users = [
+    {
+        userName: "zubair",
+        userEmail: "zubair@gmail.com",
+        userPassword: 222
+    }
+]
+var express = require("express");
+var cors = require('cors')
+var morgan = require('morgan')
+const PORT = process.env.PORT || 5000
+var bodyParser = require('body-parser')
+var app = express();
+
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(bodyParser.json())  
+
+
+app.get("/", (req,res,next)     =>{
+    
+})
